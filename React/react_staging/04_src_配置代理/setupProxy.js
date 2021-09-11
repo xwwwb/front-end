e172c0eb1702ec20489c27f7d9d4128c49d3b849
xwwwb <geekxwb@gmail.com>
2021-09-11 22:58:36 +0800

    react代理

---
+++ React/react_staging/04_src_配置代理/setupProxy.js
@@ -0,0 +1,18 @@
+const proxy = require('http-proxy-middleware')
+
+module.exports = function (app){
+app.use(
+   proxy('/api1',{ //遇见api1的请求就会触发该代理配置
+      target : 'http://localhost:5000',
+      changeOrigin:false, //控制服务器收到的请求中的头Host字段的值
+      pathRewrite:{'^/api1':''} //重写请求路径
+      
+   }),
+   proxy('/api2',{ //
+      target : 'http://localhost:5001',
+      changeOrigin:true,
+      pathRewrite:{'^/api2':''},
+      
+   })
+)
+}
