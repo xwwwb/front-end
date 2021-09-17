354ed04f5d6d6614ad2a0a1e2c51dc3fb3313b75
xwwwb <geekxwb@gmail.com>
2021-09-17 17:14:08 +0800

    完成GitHub搜索案例

--- React/react_staging/07_src_github搜索案例fetch/Search/index.jsx
+++ React/react_staging/07_src_github搜索案例fetch/Search/index.jsx
@@ -2,20 +2,21 @@ import React, { Component } from 'react'
 import './index.css'
 // import axios from 'axios'
 export default class Search extends Component {
-   search = ()=>{
+   // search = () => {
+      search = async () => {
       console.log(this.keyWordElement)
       //下面函数存在疑问 为什么清空了err改变了state 却没有触发render
       this.props.cleanErr()
       // 连续解构赋值并重命名
-      const {keyWordElement:{value:keyWord}} = this
+      const { keyWordElement: { value: keyWord } } = this
       console.log(keyWord)
-      this.props.updateAppState({isFirst:false,isLoading:true})
+      this.props.updateAppState({ isFirst: false, isLoading: true })
       //#region
       // 使用axios发送请求
       // axios.get(`/api1/search/users?q=${keyWord}`).then(
       //    response =>{console.log(response.data)
       //       this.props.updateAppState({isLoading:false,users:response.data.items})
-        
+
       //    },
       //    error =>{console.log(error)
       //       // 要存错误对象的一个属性 不能存整个对象 不然无法解析
@@ -26,22 +27,42 @@ export default class Search extends Component {
       //#endregion
 
       //使用fetch发送请求
-      fetch(`/api1/search/users?q=${keyWord}`).then(
-         response=>{console.log('成功了',response)
-         return response.json()
-      },
-         error =>{console.log('失败了',error)}
-      ).then(
-         response => console.log('成功了',response),
-         error =>{console.log(('失败了',error))}
-      )
+      // 未优化版本
+      // fetch(`/api1/search/users?q=${keyWord}`).then(
+      //    response=>{console.log('成功了',response)
+      //    return response.json()
+      // },
+      //    error =>{console.log('失败了',error)
+      //    return new Promise(()=>{})
+      // }
+      // ).then(
+      //    response => console.log('成功了',response),
+      //    error =>{console.log(('失败了',error))}
+      // )
+
+      // 优化版本
+      try{
+         const response= await fetch(`/api1/search/users?q=${keyWord}`)
+         const data = await response.json()
+         console.log(data)
+         this.props.updateAppState({isLoading:false,users:data.items})
+      }
+      catch(error){
+         console.log(error)
+         this.props.updateAppState({isLoading:false,err:error.message})
+      }
+   
+   
+   
    }
+
+
    render() {
       return (
          <section className="jumbotron">
             <h3 className="jumbotron-heading">Search Github Users</h3>
             <div>
-               <input ref={c=>this.keyWordElement=c} type="text" placeholder="enter the name you search" />&nbsp;
+               <input ref={c => this.keyWordElement = c} type="text" placeholder="enter the name you search" />&nbsp;
                <button onClick={this.search}>Search</button>
             </div>
          </section>
