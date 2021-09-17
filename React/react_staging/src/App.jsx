d7e77999750e0b70e396417192ced2ed2c409a4b
xwwwb <geekxwb@gmail.com>
2021-09-17 16:22:48 +0800

    使用fetch

---
+++ React/react_staging/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+// import './App.css'
+import Search from './Search'
+import List from './List'
+export default class App extends Component {
+	state={
+		users:[],
+		isFirst:true,
+		isLoading:false,
+		err:''
+	}
+	updateAppState=(stateObj)=>{
+		this.setState(stateObj)
+	}
+	cleanErr=()=>{
+		this.setState({err:''})
+	}
+	 render() {
+			return (
+				 <div>
+						<div className="container">
+							 <Search cleanErr={this.cleanErr}  updateAppState={this.updateAppState}/>
+								<List users={this.state} />
+						</div>
+				 </div>
+			)
+	 }
+}
---
+++ React/react_staging/src/List/index.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export default class List extends Component {
+
+   render() {
+      const {users,isFirst,isLoading,err}=this.props.users
+      if(isFirst){
+         return <h2>欢迎使用</h2>
+      }
+      else if(isLoading){
+         return <h2>加载中 请稍后</h2>
+      }
+      else if(err.length > 0 ){
+         return <h2>发生错误{err}</h2>
+      }
+      else{
+      return (
+         <div className="row">
+            {
+              users.map((userObj)=>{
+                  return (
+                     <div key={userObj.id} className="card">
+                     <a href={userObj.html_url} target="_blank"  rel="noreferrer">
+                        <img alt="avatar" src={userObj.avatar_url} style={{ width: '100px' }} />
+                     </a>
+                     <p className="card-text">{userObj.login}</p>
+                  </div>
+                  )
+               })
+            }
+        </div>
+      )
+         }
+   }
+}
