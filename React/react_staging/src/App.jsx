12beffb37a5917de98d19ec6b1f7c988af273ff6
xwwwb <geekxwb@gmail.com>
2021-09-20 01:26:01 +0800

    初次学习路由

--- React/react_staging/src/App.jsx
+++ React/react_staging/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-
+import {Link,BrowserRouter,Route} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
 export default class App extends Component {
 	render() {
 		return (
@@ -12,14 +14,25 @@ export default class App extends Component {
 				<div className="row">
 					<div className="col-xs-2 col-xs-offset-2">
 						<div className="list-group">
-							<a className="list-group-item" href="./about.html">About</a>
-							<a className="list-group-item active" href="./home.html">Home</a>
+							{/* 原生html中靠<a>跳转到不同的页面 */}
+							{/* <a className="list-group-item" href="./about.html">About</a>
+							<a className="list-group-item active" href="./home.html">Home</a> */}
+
+							{/* 在react中靠路由链接实现切换组件  编写路由链接*/}
+							
+							 <Link className="list-group-item" to="/about">About</Link>
+							 <Link className="list-group-item" to="/home">Home</Link>
+							 
 						</div>
 					</div>
 					<div className="col-xs-6">
 						<div className="panel">
 							<div className="panel-body">
-								??
+								{/* 注册路由 */}
+								
+								<Route path="/about" component={About}/>
+								<Route path="/home" component={Home}/>
+							
 							</div>
 						</div>
 					</div>
