c62c04e2ddee431756fbe799e07694740dcc9e2f
xwwwb <geekxwb@gmail.com>
2021-09-25 20:42:51 +0800

    push和replace

---
+++ React/react_staging/19_src_push和replace模式/pages/Home/Messages.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import MsgDetail from './MsgDetail'
+import { Link ,Route} from 'react-router-dom'
+export default class Messages extends Component {
+  state={
+    messageArr:[
+      {id:'01',title:'消息1'},
+      {id:'02',title:'消息2'},
+      {id:'03',title:'消息3'},
+    ]
+  }
+  render() {
+    const {messageArr}=this.state
+    return (
+      <div>
+      <ul>
+          {
+            messageArr.map((messageObj)=>{
+              return (
+                <li key={messageObj.id}>
+                  {/* param search state */}
+                {/* <Link to={`/home/messages/detail/${messageObj.id}/${messageObj.title}`}>{messageObj.title}</Link> */}
+                {/* <Link to={`/home/messages/detail/?id=${messageObj.id}&title=${messageObj.title}`}>{messageObj.title}</Link> */}
+                <Link replace={true} to={{pathname:'/home/messages/detail/',state:{id:messageObj.id,title:messageObj.title}}}>{messageObj.title}</Link>
+              </li>
+              )
+            })
+          }
+      </ul>
+      <hr />
+      {/* param search state */}
+      {/* <Route path="/home/messages/detail/:id/:title" component={MsgDetail}/> */}
+      {/* <Route path="/home/messages/detail/" component={MsgDetail}/> */}
+          <Route path="/home/messages/detail/" component={MsgDetail}/>
+    </div>
+    )
+  }
+}
---
+++ React/react_staging/19_src_push和replace模式/pages/Home/MsgDetail/index.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+// import qs from 'querystring'
+export default class MsgDetail extends Component {
+  render() {
+    console.log("@",this)
+    // const {id,title}=this.props.match.params
+    // const {search} = this.props.location
+    // 使用slice是为了截掉问号
+    // const {id,title}=qs.parse(search.slice(1))
+    const {id,title} = this.props.location.state || {}
+    return (
+      <div>
+        <ul>
+          <li>ID:{id}</li>
+          <li>Title:{title}</li>
+          <li>xx</li>
+        </ul>
+      </div>
+    )
+  }
+}
