b437c9adc80de53ac24ce0fbcfa3bca9070d3a59
xwwwb <geekxwb@gmail.com>
2021-09-26 22:24:19 +0800

    初步认识antd

--- React/react_staging/src/App.jsx
+++ React/react_staging/src/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
+import {Button} from 'antd'
+import  './App.css'
 export default class App extends Component {
 	render() {
 		return (
-			<div>
-				
+			<div style={{margin:'10px'}}>
+			   <Button type="primary">Primary Button</Button>
 			</div>
 		)
 	}
