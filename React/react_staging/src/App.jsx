1a4a4188ba759f48666f1828da45a1db3704824f
xwwwb <geekxwb@gmail.com>
2021-09-28 00:30:04 +0800

    antd的简单使用

--- React/react_staging/src/App.jsx
+++ React/react_staging/src/App.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
-import {Button} from 'antd'
+import {Button,DatePicker} from 'antd'
 import  './App.css'
+import {WechatOutlined} from '@ant-design/icons'
 export default class App extends Component {
+	onChange=(date,datestring)=>{
+console.log(date,datestring)
+	}
 	render() {
 		return (
 			<div style={{margin:'10px'}}>
 			   <Button type="primary">Primary Button</Button>
+				 <WechatOutlined/>
+				 <DatePicker onChange={this.onChange}/>
 			</div>
 		)
 	}
