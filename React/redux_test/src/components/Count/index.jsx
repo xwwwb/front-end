7589990f2b41cbdbf253f7ac7ab0bbd2b22cafe0
xwwwb <geekxwb@gmail.com>
2021-11-07 22:09:22 +0800

    接着更新react-redux

--- React/redux_test/src/components/Count/index.jsx
+++ React/redux_test/src/components/Count/index.jsx
@@ -1,42 +1,28 @@
 import React, { Component } from 'react'
-// 引入store
-import store from '../../redux/store'
-import { createIncrementAction , createDecrementAction ,
-  createIncrementAsyncAction } from '../../redux/count_action'
 export default class Count extends Component {
   state = {name:"xwb"}
 
-  // componentDidMount(){
-  //   // 监测redux中的状态变化 只要变化 就调用render
-  //   store.subscribe(()=>{
-  //     this.setState({})
-  //   })
-  // }
-
   increment = ()=>{
     const {value} = this.selectNumber
-    store.dispatch(createIncrementAction(value*1))
+  
   }
   decrement = ()=>{
     const {value} = this.selectNumber
-    store.dispatch(createDecrementAction(value*1))
+  
   }
   incrementIfOdd = ()=>{
     const {value} = this.selectNumber
-    const count = store.getState()
-    if (count %2 !==0){
-      store.dispatch(createIncrementAction(value*1))
-    }
+
   }
   incrementAsync = ()=>{
     const {value} = this.selectNumber
-    store.dispatch(createIncrementAsyncAction(value*1,500))
+
   }
 
   render() {
     return (
       <div>
-        <h1>当前求和为：{store.getState()}</h1>
+        <h1>当前求和为：{}</h1>
         <select ref={c=>{this.selectNumber=c}}>
           <option value="1">1</option>
           <option value="2">2</option>
---
+++ React/redux_test/src/containers/Count/index.jsx
@@ -0,0 +1,8 @@
+// 引入Count的UI组件
+import CountUI from '../../components/Count'
+// 引入redux
+import store from '../../redux/store'
+// 引入connect用于连接UI组件与redux
+import {connect} from 'react-redux'
+
+connect()(   )
