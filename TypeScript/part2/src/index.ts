ddf5b6ffc3ecfbad0d158d3a182c8ed1ddc3a4aa
xwwwb <geekxwb@gmail.com>
2021-11-17 15:03:53 +0800

    typescript配置文件更新完成

--- TypeScript/part2/src/index.ts
+++ TypeScript/part2/src/index.ts
@@ -1,3 +1,31 @@
 import { a } from './app'
 let b = 1;
 console.log(a)
+
+// 如果形式参数只写(a,b)会触发noImplicitAny报错
+function fun1(a:number,b:number){
+  return a+b;
+}
+
+// 这里this不明确 需要指定类型 会触发noImplicitThis
+// function fun2(this:any){
+//   alert(this)
+// }
+
+function fun2(this:Window){
+  alert(this)
+}
+
+let box= document.getElementById('root')
+
+// 如果不用这个if 就会使用strictNullChecks检查报错
+if(box != null){
+  box.addEventListener('click',()=>{
+    alert('box');
+  })
+}
+
+// 或者
+box?.addEventListener('click',()=>{
+  alert('box')
+})
