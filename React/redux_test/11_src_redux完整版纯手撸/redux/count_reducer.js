4415a91a2ef6470b413aca6ddd0fd658d13db0e7
xwwwb <xuwenbo@88.com>
2022-03-04 17:27:41 +0800

    全部引入常量值

--- React/redux_test/11_src_redux完整版纯手撸/redux/count_reducer.js
+++ React/redux_test/11_src_redux完整版纯手撸/redux/count_reducer.js
@@ -1,15 +1,15 @@
 // reducer 是一个函数 由store调用 传入一个之前的state值 一个动作和数据
-
+import { DECREAMENT ,INCREAMENT } from "./constants"
 const initState = 0
 export default function countReducer(preState, action) {
 	console.log(preState, action) // 第一次打开页面的时候 preState是undefined action是 @@init@@xxx 初始化
 	const { type, value } = action
 	if (preState === undefined) preState = initState
 	switch (type) {
-		case "increament":
+		case INCREAMENT:
 			console.log(preState, value)
 			return preState + value
-		case "decreament":
+		case DECREAMENT:
 			return preState - value
 		default:
 			return preState
