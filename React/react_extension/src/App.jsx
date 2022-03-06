855b111fa085f8917dc2a553a13f122433cfffcd
xwwwb <xuwenbo@88.com>
2022-03-06 20:47:06 +0800

    Fragment 提交

--- React/react_extension/src/App.jsx
+++ React/react_extension/src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Demo from './components/1_setState'
 import Demo2 from './components/2_lazyLoad'
 import Demo3 from './components/3_hooks'
+import Demo4 from './components/4_Fragment'
 import { BrowserRouter } from 'react-router-dom'
 
 export default class App extends Component {
@@ -9,8 +10,11 @@ export default class App extends Component {
     // return (
     //   <Demo demo={101}/>
     // )
-   return(
-     <Demo3>xxx</Demo3>
-   )
-  }
+  //  return(
+  //    <Demo3>xxx</Demo3>
+  //  )
+    return(
+      <Demo4></Demo4>
+    )
+}
 }
---
+++ React/react_extension/src/components/4_Fragment/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react'
+
+export default class index extends Component {
+  render() {
+    return (
+      <Fragment>
+      <input type="text" />
+      <input type="text" />
+      </Fragment>
+    )
+  }
+}
