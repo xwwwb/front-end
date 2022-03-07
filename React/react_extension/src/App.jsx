bd174fa70b84bd2d1ef3e5f808a3b7406b0cd98f
xwwwb <xuwenbo@88.com>
2022-03-07 12:48:44 +0800

    更新context

--- React/react_extension/src/App.jsx
+++ React/react_extension/src/App.jsx
@@ -3,6 +3,7 @@ import Demo from './components/1_setState'
 import Demo2 from './components/2_lazyLoad'
 import Demo3 from './components/3_hooks'
 import Demo4 from './components/4_Fragment'
+import Demo5 from './components/5_Context'
 import { BrowserRouter } from 'react-router-dom'
 
 export default class App extends Component {
@@ -10,11 +11,14 @@ export default class App extends Component {
     // return (
     //   <Demo demo={101}/>
     // )
-  //  return(
-  //    <Demo3>xxx</Demo3>
-  //  )
-    return(
-      <Demo4></Demo4>
+    // return(
+    //   <Demo3>xxx</Demo3>
+    // )
+    // return(
+    //   <Demo4></Demo4>
+    // )
+       return(
+      <Demo5></Demo5>
     )
 }
 }
---
+++ React/react_extension/src/components/5_Context/index.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react"
+
+const UserNameContext = React.createContext()
+
+export default class A extends Component {
+	state = { username: "tom" }
+	render() {
+		return (
+			<div>
+				<h3>我是A组件</h3>
+				<h4>我的用户名是{this.state.username}</h4>
+				<UserNameContext.Provider
+					value={{ username: this.state.username, age: 18 }}
+				>
+					<B />
+				</UserNameContext.Provider>
+			</div>
+		)
+	}
+}
+class B extends Component {
+	render() {
+		return (
+			<div>
+				<div>我是B组件</div>
+				<h4>我从A组件接收到的用户名是？？</h4>
+				<C />
+			</div>
+		)
+	}
+}
+
+// class C extends Component {
+// 	// 声明一下
+// 	static contextType = UserNameContext
+// 	render() {
+// 		console.log(this)
+// 		return (
+// 			<div>
+// 				<div>我是C 组件</div>
+// 				<h4>我从A组件接收到的用户名是{this.context.username}{this.context.age}</h4>
+// 				<D/>
+// 			</div>
+// 		)
+// 	}
+
+function C() {
+	return (
+		<div>
+			<div>我是C 组件</div>
+			<h4>
+				我从A组件接收到的用户名是
+				<UserNameContext.Consumer>
+					{
+						value=>{
+							return `${value.username},年龄是${value.age}`
+						}
+					}
+				</UserNameContext.Consumer>
+			</h4>
+		</div>
+	)
+}
