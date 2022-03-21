fa8fd37bf0e9fa716998040d13e5fc753247f01a
xwwwb <xuwenbo@88.com>
2022-03-21 22:23:32 +0800

    手写promise更新

--- Promise/03_手写Promise/01_初始结构搭建/promise.js
+++ Promise/03_手写Promise/01_初始结构搭建/promise.js
@@ -7,6 +7,8 @@ function Promise(executor) {
 	// 保存实例对象的this值
 	const self = this
 	function resolve(data) {
+		// 判断状态
+		if (self.PromiseState !== "pending") return
 		console.log(this) // 这里为什么是window
 		// 1.修改对象的状态(promiseState)
 		self.PromiseState = "fulfilled"
@@ -14,10 +16,30 @@ function Promise(executor) {
 		self.PromiseResult = data
 	}
 	function reject(data) {
+		if (self.PromiseState !== "pending") return
+		console.log(this) // 这里为什么是window
 		self.PromiseState = "rejected"
 		self.PromiseResult = data
 	}
-	executor(resolve, reject)
+	try {
+		executor(resolve, reject)
+	} catch (e) {
+		reject(e)
+	}
 }
 
-Promise.prototype.then = function (onResolved, onRejected) {}
+Promise.prototype.then = function (onResolved, onRejected) {
+	console.log("开始执行then了")
+	if (this.PromiseState === "fulfilled") {
+		onResolved(this.PromiseResult)
+	}
+	if (this.PromiseState === "rejected") {
+		onRejected(this.PromiseResult)
+	}
+	// if (this.PromiseState === "pending") {
+	// 	console.log("这里不对 这里的promise实例包裹的异步任务 但是会先执行then")
+	// }
+	if(this.PromiseState === 'pending'){
+		
+	}
+}
