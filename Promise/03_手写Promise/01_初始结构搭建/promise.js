2559b8306e0df2cb6c58a7fbf809e273742ae5fa
xwwwb <xuwenbo@88.com>
2022-03-21 19:03:17 +0800

    手写promise

--- Promise/03_手写Promise/01_初始结构搭建/promise.js
+++ Promise/03_手写Promise/01_初始结构搭建/promise.js
@@ -1,14 +1,23 @@
-function Promise(executor){
-  // 同步调用 执行器函数
-  function resolve(data){
+function Promise(executor) {
+	// 同步调用 执行器函数
 
-  }
-  function reject(data){
-
-  }
-  executor(resolve,reject);
+	// 添加属性
+	this.PromiseState = "pending"
+	this.PromiseResult = null
+	// 保存实例对象的this值
+	const self = this
+	function resolve(data) {
+		console.log(this) // 这里为什么是window
+		// 1.修改对象的状态(promiseState)
+		self.PromiseState = "fulfilled"
+		// 2.设置对象结果值(promiseResult)
+		self.PromiseResult = data
+	}
+	function reject(data) {
+		self.PromiseState = "rejected"
+		self.PromiseResult = data
+	}
+	executor(resolve, reject)
 }
 
-Promise.prototype.then = function(onResolved,onRejected){
-
-}
+Promise.prototype.then = function (onResolved, onRejected) {}
