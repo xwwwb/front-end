4e107bd1db4469fa9da8c5cd225796cec66bd93c
xwwwb <xuwenbo@88.com>
2022-04-22 20:15:43 +0800

    react笔记更新

--- React/react_extension/src/components/7_renderProps/index.jsx
+++ React/react_extension/src/components/7_renderProps/index.jsx
@@ -3,8 +3,31 @@ import './index.css'
 export default class Parent extends Component {
   render() {
     return (
-      <div>
-        <h3 className='parent'>我是parent组件</h3>
+      <div className='parent'>
+        <h3>我是parent组件</h3>
+        <A />
+      </div>
+    )
+  }
+}
+
+class A extends Component {
+  render() {
+    return (
+      <div className='child'>
+        <h3 >我是A组件
+        </h3>
+        <B />
+      </div>
+    )
+  }
+}
+
+class B extends Component {
+  render() {
+    return (
+      <div className='child_2'>
+        <h3>我是B组件</h3>
       </div>
     )
   }
