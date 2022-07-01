8f2f3851ae4fcdce0d89ecfdc310c3a859a647d0
xwwwb <xuwenbo@88.com>
2022-07-01 21:15:36 +0800

    清理项目目录

--- TypeScript/part1/02_basis.js
+++ TypeScript/part1/02_basis.js
@@ -2,9 +2,9 @@
 var a;
 // a的类型设置为了number 在以后的使用过程中a的值只能是数字
 a = 10;
-// a = 'hello';  //此行代码会报错 因为a的类型是number 不能赋值字符串
+a = "hello"; //此行代码会报错 因为a的类型是number 不能赋值字符串
 var b;
-b = 'hello';
+b = "hello";
 // b = 10
 // 声明完变量直接进行赋值
 var c = false;
@@ -16,5 +16,5 @@ var d = false;
 function sum(a, b) {
     return a + b;
 }
-// sum(123,'123')  
+// sum(123,'123')
 var result = sum(123, 456);
--- TypeScript/part1/02_basis.ts
+++ TypeScript/part1/02_basis.ts
@@ -1,29 +1,29 @@
 // 声明一个变量a 同时指定它的类型为number
-let a: number;
+let a: number
 
 // a的类型设置为了number 在以后的使用过程中a的值只能是数字
-a = 10;
-// a = 'hello';  //此行代码会报错 因为a的类型是number 不能赋值字符串
+a = 10
+// a = "hello" //此行代码会报错 因为a的类型是number 不能赋值字符串
 
-let b :string;
+let b: string
 
-b='hello'
+b = "hello"
 
 // b = 10
 
 // 声明完变量直接进行赋值
-let c:boolean = false;
+let c: boolean = false
 
-c = true;
+c = true
 // c = 123;
 
 // 如果变量声明和赋值同时进行 ts可以自动对变量类型检测
-let d = false;
+let d = false
 // d = 123
 
-function sum(a:number,b:number) : number{
-  return a+b;
+function sum(a: number, b: number): number {
+	return a + b
 }
 
-// sum(123,'123')  
-let result = sum(123,456)
+// sum(123,'123')
+let result: number = sum(123, 456)
