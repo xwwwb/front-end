917040fecefdcd6e376ca454c10bfd10d65fea32
xwwwb <xuwenbo@88.com>
2022-07-03 00:37:08 +0800

    更新ts笔记

--- TypeScript/part2/dist/app.js
+++ TypeScript/part2/dist/app.js
@@ -1,2 +1,30 @@
 "use strict";
-console.log('hehe');
+define("app", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.a = void 0;
+    console.log('hello');
+    exports.a = 10;
+});
+define("index", ["require", "exports", "app"], function (require, exports, app_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    let b = 1;
+    console.log(app_1.a);
+    function fun1(a, b) {
+        return a + b;
+    }
+    function fun2() {
+        alert(this);
+    }
+    let box = document.getElementById('root');
+    if (box != null) {
+        box.addEventListener('click', () => {
+            alert('box');
+        });
+    }
+    box?.addEventListener('click', () => {
+        alert('box');
+    });
+});
+console.log("hello");
--- TypeScript/part2/src/index.ts
+++ TypeScript/part2/src/index.ts
@@ -1,10 +1,10 @@
-import { a } from './app'
-let b = 1;
+import { a } from "./app"
+let b = 1
 console.log(a)
 
 // 如果形式参数只写(a,b)会触发noImplicitAny报错
-function fun1(a:number,b:number){
-  return a+b;
+function fun1(a: number, b: number): number {
+	return a + b
 }
 
 // 这里this不明确 需要指定类型 会触发noImplicitThis
@@ -12,20 +12,20 @@ function fun1(a:number,b:number){
 //   alert(this)
 // }
 
-function fun2(this:Window){
-  alert(this)
+function fun2(this: Window) {
+	alert(this)
 }
 
-let box= document.getElementById('root')
+let box = document.getElementById("root")
 
 // 如果不用这个if 就会使用strictNullChecks检查报错
-if(box != null){
-  box.addEventListener('click',()=>{
-    alert('box');
-  })
+if (box != null) {
+	box.addEventListener("click", () => {
+		alert("box")
+	})
 }
 
 // 或者
-box?.addEventListener('click',()=>{
-  alert('box')
+box?.addEventListener("click", () => {
+	alert("box")
 })
